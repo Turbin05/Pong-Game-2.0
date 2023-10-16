2148e73ba3e0bb4a42401a4f989cb68ad2411bb9
Tural Binnatov <89636414+Turbin05@users.noreply.github.com>
2023-10-16 08:45:16 +0200

    Paddle.rb

---
+++ Paddle.rb
@@ -0,0 +1,12 @@
+require 'gosu'
+
+class Paddle < GameObject
+    WIDTH = 12
+    HEIGHT = 60
+    def initialize(x, y)
+      super(x, y, WIDTH, HEIGHT)
+    end
+    def draw
+      Gosu.draw_rect x, y, w, h, Gosu::Color::WHITE
+    end
+end
